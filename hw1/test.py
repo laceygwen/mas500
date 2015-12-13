5b65f14c13b00bb298064123d0ed496bf03e758d
laceygwen <LaceyG@Laceys-MacBook-Pro.local>
2015-12-13 14:38:04 -0500

    Add unit test and logging
    for media cloud api call

---
+++ hw1/test.py
@@ -0,0 +1,14 @@
+# We forked hw1 intermediate from Jasmin Rubinovitz.
+
+import logging
+import unittest
+import hw1
+
+class mcAPItest(unittest.TestCase):
+
+    def mcAPItest (self):
+    	res = hw1
+    	assert res != None
+
+if __name__ == '__main__':
+    unittest.main()
